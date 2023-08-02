0f07a913528bcfe948506d4c0b381d849e86a604
Buğra Canata <bcanata@hotmail.com>
2023-08-02 23:53:01 +0300

    removed broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/notlar">
+            to="/notlar/intro">
             Okumaya Başlayın!
           </Link>
         </div>
